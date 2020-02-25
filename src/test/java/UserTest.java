d78f25cca5ab82d2b34696fc9000b126e37fc241
Eathan Langford <eathan.langford@myob.com>
2020-02-25 16:13:01 +1100

    set up User tests

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,13 +1,37 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
+    User actual;
+
+    @Before
+    public void setUp() {
+        actual = new User("Eathan", "Langford", 65000, 9);
+    }
 
     @Test
     public void shouldCreateUserWithFirstName_WhenConstructed() {
-        User actual = new User("Eathan", "Langford", 65000, 9);
         String expected = "Eathan";
 
-        Assert.assertEquals(actual.getName(), expected);
+        Assert.assertEquals(actual.getFirstName(), expected);
+    }
+    @Test
+    public void shouldCreateUserWithLastName_WhenConstructed() {
+        String expected = "Langford";
+
+        Assert.assertEquals(actual.getLastName(), expected);
+    }
+    @Test
+    public void shouldCreateUserWithFirstName_WhenConstructed() {
+        int expected = 65000;
+
+        Assert.assertEquals(actual.getSalary(), expected);
+    }
+    @Test
+    public void shouldCreateUserWithFirstName_WhenConstructed() {
+        int expected = 9;
+
+        Assert.assertEquals(actual.getSuperRate(), expected);
     }
 }
