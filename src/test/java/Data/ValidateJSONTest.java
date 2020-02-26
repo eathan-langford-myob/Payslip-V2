bb1b3226dc3acc301f763e8c904681ba2728fec1
Eathan Langford <eathan.langford@myob.com>
2020-02-27 00:32:58 +1100

    passed json validation test

--- src/test/java/Data/ValidateJSONTest.java
+++ src/test/java/Data/ValidateJSONTest.java
@@ -8,8 +8,47 @@ public class ValidateJSONTest {
     @Test
     public void shouldReturnTrueForValidJSON() {
         ValidateJSON validator = new ValidateJSON();
-        boolean actual = validator.isValidJSON("2017-18_Tax_Data.JSON");
+        boolean actual = validator.isValidJSON("./src/main/resources/2017-18_Tax_Data.JSON");
 
         Assert.assertTrue(actual);
     }
+
+    @Test
+    public void shouldReturnTrue_WhenGivenValidStringNumber() {
+        ValidateJSON validator = new ValidateJSON();
+        boolean actual = validator.isValidDigit("55");
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void shouldReturnFalse_WhenGivenInvalidStringNumber() {
+        ValidateJSON validator = new ValidateJSON();
+        boolean actual = validator.isValidDigit(".55");
+
+        Assert.assertFalse(actual);
+    }
+    @Test
+    public void shouldReturnTrue_WhenGivenValidStringFloat() {
+        ValidateJSON validator = new ValidateJSON();
+        boolean actual = validator.isValidDigit("0.55");
+
+        Assert.assertTrue(actual);
+    }
+    @Test
+    public void shouldReturnTrue_WhenGivenValidStringNegative() {
+        ValidateJSON validator = new ValidateJSON();
+        boolean actual = validator.isValidDigit("-55");
+
+        Assert.assertTrue(actual);
+    }
+    @Test
+    public void shouldReturnTrue_WhenGivenZero() {
+        ValidateJSON validator = new ValidateJSON();
+        boolean actual = validator.isValidDigit("0");
+
+        Assert.assertTrue(actual);
+    }
+
+
 }
