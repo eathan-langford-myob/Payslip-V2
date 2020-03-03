d21dc23c2939a196a7f3b676a48c909b35fa5149
Eathan Langford <eathan.langford@myob.com>
2020-03-03 11:28:52 +1100

    added date validator test

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,6 +1,10 @@
 public class InputValidator {
 
     public static boolean isValidDigit(String salary) {
+        return salary.chars().allMatch(Character::isDigit);
+    }
 
+    public static boolean isValidDateFormat(String startDate) {
+        return true;
     }
 }
